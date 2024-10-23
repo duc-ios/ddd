592670b47b62e25afdaa25943d2e006cd42725c1
Duc Nguyen <duc.nh2012@gmail.com>
2024-10-23 23:39:36 +0700

    refactor: update cancelable_bloc_base & api_error imports

--- bricks/ddd/__brick__/{{name.snakeCase()}}/application/{{name.snakeCase()}}_cubit/{{name.snakeCase()}}_cubit.dart
+++ bricks/ddd/__brick__/{{name.snakeCase()}}/application/{{name.snakeCase()}}_cubit/{{name.snakeCase()}}_cubit.dart
@@ -3,8 +3,8 @@ import 'package:injectable/injectable.dart';
 {{#is_bloc}}import 'package:bloc/bloc.dart';{{/is_bloc}}{{#is_riverpod}}import 'package:riverbloc/riverbloc.dart';
 import '../../../../common/utils/getit_utils.dart';{{/is_riverpod}}
 
-import '../../../../common/mixin/cancelable_base_bloc.dart';
-import '../../../../core/infrastructure/datasources/remote/api/base/api_error.dart';
+import '../../../../common/mixin/cancelable_bloc_base.dart';
+import '../../../../core/domain/errors/api_error.dart';
 import '../../domain/entities/{{name.snakeCase()}}.dart';
 import '../../domain/repositories/{{name.snakeCase()}}_repository.dart';
 
@@ -16,7 +16,7 @@ part '{{name.snakeCase()}}_cubit.freezed.dart';
 });{{/is_riverpod}}
 
 @injectable
-class {{name.pascalCase()}}Cubit extends Cubit<{{name.pascalCase()}}State> with CancelableBaseBloc {
+class {{name.pascalCase()}}Cubit extends Cubit<{{name.pascalCase()}}State> with CancelableBlocBase {
   final {{name.pascalCase()}}Repository _repository;
 
   {{name.pascalCase()}}Cubit(this._repository) : super(const {{name.pascalCase()}}State());
